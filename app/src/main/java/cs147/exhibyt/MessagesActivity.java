825654463f6305c9ff64fe718957786458c98f30
Tara Iyer <tiyer@stanford.edu>
2018-11-30 14:10:00 -0800

    Added a hardcoded representation of the messages activity and fixed nav bar links

--- app/src/main/java/cs147/exhibyt/MessagesActivity.java
+++ app/src/main/java/cs147/exhibyt/MessagesActivity.java
@@ -17,56 +17,10 @@ public class MessagesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        //Kudos to https://developer.android.com/reference/android/support/design/widget/FloatingActionButton
-        // for its help
-        /*FloatingActionButton fab = findViewById(R.id.addQ);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println("SUCCESSFUL!");
-            }
-        });*/
-
-
-
-        ArrayList<Question> Qarray = new ArrayList<>();
-        Question q = new Question("What is the best part of this picture?");
-        Qarray.add(new Question("What is the best part of this picture?"));
-        Qarray.add(new Question("What is the worst part of this picture?"));
-        Qarray.add(new Question("What is scary?"));
-        Qarray.add(new Question("What do you like about this picture?"));
-        Qarray.add(new Question("Moo"));
-        Qarray.add(new Question("What is the best part of this picture?"));
-        Qarray.add(new Question("What is the worst part of this picture?"));
-        Qarray.add(new Question("What is scary?"));
-        Qarray.add(new Question("What do you like about this picture?"));
-        Qarray.add(new Question("Moo"));
-        Qarray.add(new Question("What is the best part of this picture?"));
-        Qarray.add(new Question("What is the worst part of this picture?"));
-        Qarray.add(new Question("What is scary?"));
-        Qarray.add(new Question("What do you like about this picture?"));
-        Qarray.add(new Question("Moo"));
-        Qarray.add(new Question("What is the best part of this picture?"));
-        Qarray.add(new Question("What is the worst part of this picture?"));
-        Qarray.add(new Question("What is scary?"));
-        Qarray.add(new Question("What do you like about this picture?"));
-        Qarray.add(new Question("Moo"));
-        Qarray.add(new Question("What is the best part of this picture?"));
-        Qarray.add(new Question("What is the worst part of this picture?"));
-        Qarray.add(new Question("What is scary?"));
-        Qarray.add(new Question("What do you like about this picture?"));
-        Qarray.add(new Question("Moo"));
-
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
         setTitle("Messages");
-        ListView Qlist = (ListView) findViewById(R.id.listofQs);
-        QuestionsAdapter adapter = new QuestionsAdapter(
-                this,
-                R.layout.question_view,
-                Qarray);
-        Qlist.setAdapter(adapter);
     }
 
 
