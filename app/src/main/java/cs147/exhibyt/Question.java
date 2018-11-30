88b8d80e1dffcd97334f32067652fc683dbaf539
Christina <capan@stanford.edu>
2018-11-30 01:01:29 -0800

    Changed questions to include more information about pictures and responses

--- app/src/main/java/cs147/exhibyt/Question.java
+++ app/src/main/java/cs147/exhibyt/Question.java
@@ -1,19 +1,40 @@
 package cs147.exhibyt;
 
+import java.util.ArrayList;
+
 /**
  * Created by Tara on 11/24/2018.
  */
 
 public class Question {
     private String questionText;
+    private ArrayList<Response> responses;
+    private ArrayList<String> imageNames;
+
     // There should be a list of images
     // There should also be an id
 
     public Question(String questionText) {
         this.questionText = questionText;
+        this.responses = new ArrayList<>();
+        this.imageNames = new ArrayList<>();
+    }
+
+    public Question(String questionText, ArrayList<String> imageNames) {
+        this.questionText = questionText;
+        this.responses = new ArrayList<>();
+        this.imageNames = imageNames;
     }
 
     public String getQText(){
         return this.questionText;
+    }
+
+    public ArrayList<Response> getResponses() {
+        return this.responses;
+    }
+
+    public ArrayList<String> getArtworks(){
+        return this.imageNames;
     }
 }
