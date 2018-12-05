9a02c0266eac90e39aec4f76efd73964a0165f72
Christina <capan@stanford.edu>
2018-12-04 23:51:31 -0800

    Added frame background picture

--- app/src/main/java/cs147/exhibyt/AddQuestionActivity.java
+++ app/src/main/java/cs147/exhibyt/AddQuestionActivity.java
@@ -1,7 +1,13 @@
 package cs147.exhibyt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class AddQuestionActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class AddQuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_question);
+    }
+
+    public void changeOpacity(View v) {
+        if (v.getAlpha() < 1f) {
+            v.setAlpha(1);
+        } else {
+            v.setAlpha(.2f);
+        }
+    }
+
+    // Here, we're submitting the app
+    public void goToQuestionsN(View v) {
+        DataSingleton ds = DataSingleton.getInstance();
+        ArrayList<Question> qList = ds.getQuestionList();
+
+        ArrayList<String> imgs = new ArrayList<>();
+
+        //See if plane image selected
+        ImageView curr = (ImageView) findViewById(R.id.planeImage);
+        if (curr.getAlpha() == 1) imgs.add("plane");
+
+        //See if
+        curr = (ImageView) findViewById(R.id.planeImage);
+        if (curr.getAlpha() == 1) imgs.add("plane");
+        curr = (ImageView) findViewById(R.id.planeImage);
+        if (curr.getAlpha() == 1) imgs.add("plane");
+        curr = (ImageView) findViewById(R.id.planeImage);
+        if (curr.getAlpha() == 1) imgs.add("plane");
+
+        Question newQ = new Question();
+
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
     }
 }
